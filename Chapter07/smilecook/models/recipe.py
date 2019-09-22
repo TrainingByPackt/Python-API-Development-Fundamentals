54103f382aadb0646cfdc971a86b7ffd7d1a3bfa
raychung <raychung.tw@gmail.com>
2019-09-23 00:34:26 +0800

    add chapter07

---
+++ Chapter07/smilecook/models/recipe.py
@@ -0,0 +1,47 @@
+from extensions import db
+
+
+class Recipe(db.Model):
+    __tablename__ = 'recipe'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    num_of_servings = db.Column(db.Integer)
+    cook_time = db.Column(db.Integer)
+    directions = db.Column(db.String(1000))
+    is_publish = db.Column(db.Boolean(), default=False)
+    cover_image = db.Column(db.String(100), default=None)
+
+    created_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now(), onupdate=db.func.now())
+
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
+
+    @classmethod
+    def get_all_published(cls):
+        return cls.query.filter_by(is_publish=True).all()
+
+    @classmethod
+    def get_all_by_user(cls, user_id, visibility='public'):
+        if visibility == 'public':
+            return cls.query.filter_by(user_id=user_id, is_publish=True).all()
+
+        elif visibility == 'private':
+            return cls.query.filter_by(user_id=user_id, is_publish=False).all()
+
+        else:
+            return cls.query.filter_by(user_id=user_id).all()
+
+    @classmethod
+    def get_by_id(cls, recipe_id):
+        return cls.query.filter_by(id=recipe_id).first()
+
+    def save(self):
+
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()

bb88782b1639b8a64f6f7bdfbe57e6a96f34a646
raychung <raychung.tw@gmail.com>
2019-11-03 19:10:39 +0800

    add Lesson03

---
+++ Lesson03/Exercise19/models/user.py
@@ -0,0 +1,27 @@
+from extensions import db
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    email = db.Column(db.String(200), nullable=False, unique=True)
+    password = db.Column(db.String(200))
+    is_active = db.Column(db.Boolean(), default=False)
+    created_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now(), onupdate=db.func.now())
+
+    recipes = db.relationship('Recipe', backref='user')
+
+    @classmethod
+    def get_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def get_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
---
+++ Lesson03/Exercise22/app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+
+from config import Config
+from extensions import db
+
+from resources.user import UserListResource
+from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    register_extensions(app)
+    register_resources(app)
+
+    return app
+
+
+def register_extensions(app):
+    db.app = app
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+
+def register_resources(app):
+    api = Api(app)
+
+    api.add_resource(UserListResource, '/users')
+    api.add_resource(RecipeListResource, '/recipes')
+    api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
+    api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
